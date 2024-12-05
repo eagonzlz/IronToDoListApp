4bc2c4bdcfbd20a4556ac964efc931554c00d90a
eagonzlz <101129923+eagonzlz@users.noreply.github.com>
2024-12-05 08:35:11 -0400

    Initial Commit

---
+++ MyToDoList/MyToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoListApp.swift
+//  MyToDoList
+//
+//  Created by ArcaneHollow on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
